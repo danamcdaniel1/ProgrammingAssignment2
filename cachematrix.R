f9ce561f334d1aa15ee36f130ac9d8529291b7e5
danamcdaniel1 <dana.mcdaniel1@gmail.com>
2014-04-27 01:37:51 -0700

    assignment completed to solve for invertible matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Strategy
+# part i.  use makeCacheMatrix() to create useful functions and variables
+# part ii. use cacheSolve to get inverse variable in part i.  if not there, 
+# part iii.  use cacheSolve for the inverse of the matrix.
+# part iv. cacheSolve sets makeCacheMatrix inverse value for the future.
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix
+# instantiate 'i' the variable for the inverse matrix
+# instantiate list of get/set functions
 
+makeCacheMatrix <- function(x = matrix()) { #AKA mCM
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }  ## end set
+    
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    
+    # mCM returns a list of functions, for reference
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+# refers to functions, variable 'i' in makeCacheMatrix
+# gets inverse 'i', if no inverse exists, solves and sets inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    matrix <- x$get()
+    i <- solve(matrix, ...)
+    x$setinverse(i)
+    i
 }
+
+# Implementation to check for correctness
+q <- matrix(1:4,2)
+w <- makeCacheMatrix(q)
+e <- cacheSolve(w)
+
+#Does cacheSolve give identical result as Solve()?
+identical(e,solve(q))
+
